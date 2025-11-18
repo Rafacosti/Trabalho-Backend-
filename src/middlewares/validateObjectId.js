f62ff06e183abf07f10eb210c2d6040403acbb1f
Rafael Costa <136120341+Rafacosti@users.noreply.github.com>
2025-11-17 21:20:08 -0300

    Delete src/middlewares directory

--- src/middlewares/validateObjectId.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = (param = 'id') => (req, res, next) => {
-  const value = req.params[param];
-  if (!mongoose.Types.ObjectId.isValid(value)) {
-    return res.status(400).json({ message: `Invalid ObjectId: ${param}` });
-  }
-  next();
-};
--- src/middlewares/validateYup.js
+++
@@ -1,9 +0,0 @@
-module.exports = (schema) => async (req, res, next) => {
-    try {
-      const validated = await schema.validate(req.body, { abortEarly: false, stripUnknown: true });
-      req.body = validated;
-      return next();
-    } catch (err) {
-      return res.status(400).json({ message: 'Validation failed', errors: err.errors });
-    }
-  };
